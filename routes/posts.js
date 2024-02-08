7878602d5d1e9aef4528ac71445d8430a11b0a3e
k9abhinav <k9abhinava@gmail.com>
2024-02-08 23:24:45 +0530

    NEWENVVV

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect("mongodb+srv://abhinava03kks:pinterest@pinterest.oltmm1v.mongodb.net/users/?retryWrites=true&w=majority").then((response)=>{
+mongoose.connect("mongodb+srv://abhinava03kks:pinterest@pinterest.oltmm1v.mongodb.net/?retryWrites=true&w=majority").then((response)=>{
   console.log("Connected to postModel DB")
 })
 .catch((error)=>{
